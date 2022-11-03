71829d52e73b05fb1d3a520cf0b115587f7520e1
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2022-11-03 22:37:17 +0100

    version 1.0

--- app/src/main/java/com/kadirbozkurt/mycoamail/MainActivity.java
+++ app/src/main/java/com/kadirbozkurt/mycoamail/MainActivity.java
@@ -1,16 +1,49 @@
 package com.kadirbozkurt.mycoamail;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Application;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.appopen.AppOpenAd;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.material.snackbar.Snackbar;
 
 import org.jsoup.Jsoup;
@@ -18,6 +51,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+//banner ad : ca-app-pub-8301793912429911/4443825316
+//intersititial ad: ca-app-pub-8301793912429911/8053620032
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
@@ -26,30 +62,48 @@ public class MainActivity extends AppCompatActivity {
     private String vNum;
     private Dialog vNumDialog;
     private ImageView vNumSendButton;
-    private View view;
+    private int timeToRefresh;
+    private SharedPreferences sharedPreferences;
+    private ImageView mailBox;
+    private ImageView changeVnumButton;
+    private ImageView changeUpdateTimeButton;
+    private ImageView refreshButton;
+    private AdView mAdView;
+    AdRequest adRequest;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadAd();
+        bannerAd();
+        changeVnumButton = findViewById(R.id.changeVnumButton);
+        changeUpdateTimeButton = findViewById(R.id.changeUpdateTimeButton);
+        refreshButton = findViewById(R.id.refreshButton);
+        sharedPreferences = this.getSharedPreferences("com.kadirbozkurt.mycoamail", Context.MODE_PRIVATE);
+        vNum = sharedPreferences.getString("vNum","");
+        timeToRefresh = sharedPreferences.getInt("timeToRefresh",15);
+        mailBox = findViewById(R.id.mailBox);
         textView = findViewById(R.id.textView);
         textView.setVisibility(View.GONE);
+        mailBox.setVisibility(View.INVISIBLE);
         progressBar = findViewById(R.id.progressBar);
         progressBar.setVisibility(View.GONE);
         vNumDialog = new Dialog(this);
         openDialog();
 
-
     }
 
+
     protected class MailPage extends AsyncTask<Void, Void, Void> {
-            public Elements element;
+            public Elements element= null;
             @Override
             protected Void doInBackground(Void... voids) {
                 Document document = null;
                 try {
                     document = Jsoup.connect("https://www.mycoa.nl/tr/content/posta?field_post_v_nummer_value="+vNum+"&submit_me=1").get();
-                    element = document.getElementsByAttributeValue("alt","No post");
+                    element = document.getElementsByAttributeValue("alt","Post");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -59,13 +113,19 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             protected void onPostExecute(Void unused) {
-                textView.setVisibility(View.VISIBLE);
-                progressBar.setVisibility(View.GONE);
                 super.onPostExecute(unused);
-                if (element==null){
-                    textView.setText("Yo have a mail!");
-                }else{
+                showAd();
+                textView.setVisibility(View.VISIBLE);
+                refreshButton.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.GONE);
+                mailBox.setVisibility(View.VISIBLE);
+                changeUpdateTimeButton.setVisibility(View.VISIBLE);
+                changeVnumButton.setVisibility(View.VISIBLE);
+                if(element.size()==0){
                     textView.setText("You don't have a mail!");
+                }else{
+                    textView.setText("You have a mail!");
+                    mailBox.setImageResource(R.drawable.fullmailbox);
                 }
 
             }
@@ -73,36 +133,242 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            loadAd();
             progressBar.setVisibility(View.VISIBLE);
+            mailBox.setVisibility(View.INVISIBLE);
 
         }
     }
     private void openDialog(){
-        vNumDialog.setContentView(R.layout.vnum_alert);
-        vNumDialog.setCancelable(false);
-        vNumEditText = vNumDialog.findViewById(R.id.vNumEditText);
-        vNumSendButton = vNumDialog.findViewById(R.id.vNumSendButton);
-        vNumDialog.show();
+        if (vNum.isEmpty()){
+            vNumDialog.setContentView(R.layout.vnum_alert);
+            vNumDialog.setCancelable(false);
+            vNumEditText = vNumDialog.findViewById(R.id.vNumEditText);
+            vNumSendButton = vNumDialog.findViewById(R.id.vNumSendButton);
+            vNumDialog.show();
 
-        vNumSendButton.setOnClickListener(new View.OnClickListener() {
+            vNumSendButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    vNum = vNumEditText.getText().toString();
+                    if (vNum.length()<10){
+                        Snackbar.make(findViewById(android.R.id.content).getRootView(),"V-Number must be 10 digits!",Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                            }
+                        }).show();
+                    }else{
+                        sharedPreferences.edit().putString("vNum",vNum).commit();
+                        vNumDialog.dismiss();
+                        MailPage mailPage = new MailPage();
+                        mailPage.execute();
+                        executeWorkManager();
+
+                    }
+                }
+            });
+        }else {
+            vNumDialog.dismiss();
+            MailPage mailPage = new MailPage();
+            mailPage.execute();
+            executeWorkManager();
+
+        }
+    }
+    private void executeWorkManager(){
+
+        @SuppressLint("RestrictedApi") Data data = new Data.Builder().put("vNum",vNum).build();
+        Constraints constraints  = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED).build();
+
+        WorkRequest workRequest = new PeriodicWorkRequest.Builder(UpdateStatus.class,timeToRefresh, TimeUnit.MINUTES)
+                .setConstraints(constraints)
+                .setInputData(data).build();
+        WorkManager.getInstance(MainActivity.this).enqueue(workRequest);
+    }
+    public void changeVnum(View v){
+        AlertDialog.Builder alert =new AlertDialog.Builder(MainActivity.this);
+        alert.setMessage("Your previous V-Number will be deleted. Do you want to change your V-Number?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        vNum ="";
+                        sharedPreferences.edit().putString("vNum","").commit();
+                        openDialog();
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                }).show();
+    }
+    public void selectUpdateTime(View v) {
+        Dialog timeDialog = new Dialog(MainActivity.this);
+        timeDialog.setContentView(R.layout.update_time_alert);
+        timeDialog.setCancelable(false);
+
+        RadioGroup radioGroup = timeDialog.findViewById(R.id.timeRadioGroup);
+        RadioButton minutes15 = timeDialog.findViewById(R.id.minutes15);
+        RadioButton minutes30 = timeDialog.findViewById(R.id.minutes30);
+        RadioButton minutes45 = timeDialog.findViewById(R.id.minutes45);
+        RadioButton minutes60 = timeDialog.findViewById(R.id.minutes60);
+        ImageView approveTimeButton = timeDialog.findViewById(R.id.timeApproveButton);
+        timeDialog.show();
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (minutes15.isChecked()){
+                    timeToRefresh = 15;
+                }else if (minutes30.isChecked()){
+                    timeToRefresh = 30;
+                }else if (minutes45.isChecked()){
+                    timeToRefresh = 45;
+                }else if (minutes60.isChecked()){
+                    timeToRefresh = 60;
+                }
+
+
+            }
+        });
+        
+
+        approveTimeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                vNum = vNumEditText.getText().toString();
-                if (vNum.length()<10){
-                    Snackbar.make(findViewById(android.R.id.content).getRootView(),"V-Number must be 10 digits!",Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-
-                        }
-                    }).show();
-                }else{
-                vNumDialog.dismiss();
-                MailPage mailPage = new MailPage();
-                mailPage.execute();
-                }
+                sharedPreferences.edit().putInt("timeToRefresh",timeToRefresh).commit();
+                timeDialog.dismiss();
+            }
+        });
+    }
+    public void refresh(View v){
+        refreshButton.setVisibility(View.INVISIBLE);
+        changeVnumButton.setVisibility(View.INVISIBLE);
+        textView.setVisibility(View.INVISIBLE);
+        changeUpdateTimeButton.setVisibility(View.INVISIBLE);
+        MailPage mailPage = new MailPage();
+        mailPage.execute();
+        executeWorkManager();
+    }
+    private void bannerAd(){
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
             }
         });
 
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+
+            @Override
+            public void onAdFailedToLoad(LoadAdError adError) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdImpression() {
+                // Code to be executed when an impression is recorded
+                // for an ad.
+            }
+
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+        });
+    }
+    public void loadAd(){
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        adRequest = new AdRequest.Builder().build();
+
+        InterstitialAd.load(this,"ca-app-pub-8301793912429911/8053620032", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd = interstitialAd;
+                        fulscrn();
+                        //Log.i(TAG, "onAdLoaded");
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        //Log.d(TAG, loadAdError.toString());
+                        mInterstitialAd = null;
+                    }
+                });
+
 
     }
+    public void showAd(){
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(MainActivity.this);
+        } else {
+            // Log.d("TAG", "The interstitial ad wasn't ready yet.");
+        }
+    }
+    public void fulscrn(){
+        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+            @Override
+            public void onAdClicked() {
+                // Called when a click is recorded for an ad.
+                //Log.d(TAG, "Ad was clicked.");
+            }
+
+            @Override
+            public void onAdDismissedFullScreenContent() {
+                // Called when ad is dismissed.
+                // Set the ad reference to null so you don't show the ad a second time.
+                //Log.d(TAG, "Ad dismissed fullscreen content.");
+                mInterstitialAd = null;
+            }
+
+            @Override
+            public void onAdFailedToShowFullScreenContent(AdError adError) {
+                // Called when ad fails to show.
+                //Log.e(TAG, "Ad failed to show fullscreen content.");
+                mInterstitialAd = null;
+            }
+
+            @Override
+            public void onAdImpression() {
+                // Called when an impression is recorded for an ad.
+                //Log.d(TAG, "Ad recorded an impression.");
+            }
+
+            @Override
+            public void onAdShowedFullScreenContent() {
+                // Called when ad is shown.
+                //Log.d(TAG, "Ad showed fullscreen content.");
+            }
+        });
+    }
+
+
+
 }
---
+++ app/src/main/java/com/kadirbozkurt/mycoamail/UpdateStatus.java
@@ -0,0 +1,108 @@
+package com.kadirbozkurt.mycoamail;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.work.Data;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class UpdateStatus extends Worker {
+    private String vNum;
+    private SharedPreferences sharedPreferences;
+    private Context context;
+    private boolean notificationSent;
+    private NotificationManagerCompat notificationManagerCompat;
+    private Notification notification;
+    public UpdateStatus(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        this.context = context;
+
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Data data = getInputData();
+        vNum = data.getString("vNum");
+        updateStatus();
+        return Result.success();
+    }
+
+    private void updateStatus(){
+
+        GetUpdate getUpdate = new GetUpdate();
+        getUpdate.execute();
+
+    }
+
+    private class GetUpdate extends AsyncTask<Void, Void, Void> {
+        public Elements element;
+        @Override
+        protected Void doInBackground(Void... voids) {
+            sharedPreferences = context.getSharedPreferences("com.kadirbozkurt.mycoamail",Context.MODE_PRIVATE);
+            notificationSent = sharedPreferences.getBoolean("notify",true);
+            vNum = sharedPreferences.getString("vNum","");
+            Document document = null;
+            try {
+                document = Jsoup.connect("https://www.mycoa.nl/tr/content/posta?field_post_v_nummer_value="+vNum+"&submit_me=1").get();
+                element = document.getElementsByAttributeValue("alt","Post");//No Post, Post
+                System.out.println("element = " + element);
+                System.out.println("vNum = " + vNum);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            if (element.size()>0){//>0 olarak düzelt
+
+                if (notificationSent){
+                    sendNotification("COA MAIL","You have a mail!");
+                    System.out.println("element = " + element.size());
+                    notificationSent = false;
+                    sharedPreferences.edit().putBoolean("notify",notificationSent).commit();
+                }
+
+            }else {
+                notificationSent = true;
+                sharedPreferences.edit().putBoolean("notify",notificationSent).commit();
+            }
+
+
+        }
+    }
+
+    private void sendNotification(String title,String text){
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel("not","Channel", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = context.getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"not")
+                .setSmallIcon(R.drawable.fullmailbox)
+                .setContentTitle(title)
+                .setContentText(text);
+        notification = builder.build();
+        notificationManagerCompat = NotificationManagerCompat.from(context);
+
+        notificationManagerCompat.notify(1,notification);
+    }
+}
