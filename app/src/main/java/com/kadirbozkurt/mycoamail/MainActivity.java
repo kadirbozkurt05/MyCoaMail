f8be9cd46314a1ddf866b77aa82f5f0e52a912f0
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2022-12-19 17:58:20 +0100

    19/12/2022: Bazı metdolar değişti

--- app/src/main/java/com/kadirbozkurt/mycoamail/MainActivity.java
+++ app/src/main/java/com/kadirbozkurt/mycoamail/MainActivity.java
@@ -1,16 +1,16 @@
 package com.kadirbozkurt.mycoamail;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatCheckBox;
+import androidx.core.content.ContextCompat;
 import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.NetworkType;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
-
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -35,30 +35,15 @@ import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-
-import com.google.android.gms.ads.AdError;
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.FullScreenContentCallback;
-import com.google.android.gms.ads.LoadAdError;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.google.android.gms.ads.interstitial.InterstitialAd;
-import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.material.snackbar.Snackbar;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-//banner ad : ca-app-pub-8301793912429911/4443825316
-//intersititial ad: ca-app-pub-8301793912429911/8053620032
+
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
@@ -74,17 +59,12 @@ public class MainActivity extends AppCompatActivity {
     private Button changeVnumButton;
     private Button changeUpdateTimeButton;
     private Button refreshButton;
-    private AdView mAdView;
-    AdRequest adRequest;
-    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //loadAd();
-        //bannerAd();
         changeVnumButton = findViewById(R.id.changeVnumButton);
         changeUpdateTimeButton = findViewById(R.id.changeUpdateTimeButton);
         refreshButton = findViewById(R.id.refreshButton);
@@ -93,17 +73,15 @@ public class MainActivity extends AppCompatActivity {
         timeToRefresh = sharedPreferences.getInt("timeToRefresh",60);
         mailBox = findViewById(R.id.mailBox);
         textView = findViewById(R.id.textView);
-        textView.setVisibility(View.GONE);
-        mailBox.setVisibility(View.INVISIBLE);
         progressBar = findViewById(R.id.progressBar);
-        progressBar.setVisibility(View.GONE);
+        textView.setText("Your posts are being checked...");
+        mailBox.setVisibility(View.INVISIBLE);
+
         vNumDialog = new Dialog(this);
         openDialog();
 
 
     }
-
-
     protected class MailPage extends AsyncTask<Void, Void, Void> {
             public Elements element= null;
             @Override
@@ -122,7 +100,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(Void unused) {
                 super.onPostExecute(unused);
-                //showAd();
                 textView.setVisibility(View.VISIBLE);
                 refreshButton.setVisibility(View.VISIBLE);
                 progressBar.setVisibility(View.GONE);
@@ -141,7 +118,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            //loadAd();
             progressBar.setVisibility(View.VISIBLE);
             mailBox.setVisibility(View.INVISIBLE);
 
@@ -166,9 +142,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }).show();
                     }else{
-                            ignoreBatteryOptimization();
-
-
+                        ignoreBatteryOptimization();
                         sharedPreferences.edit().putString("vNum",vNum).commit();
                         vNumDialog.dismiss();
                         MailPage mailPage = new MailPage();
@@ -231,10 +205,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 if (minutes15.isChecked()){
+                    ignoreBatteryOptimization();
                     timeToRefresh = 15;
                 }else if (minutes30.isChecked()){
+                    ignoreBatteryOptimization();
                     timeToRefresh = 30;
                 }else if (minutes45.isChecked()){
+                    ignoreBatteryOptimization();
                     timeToRefresh = 45;
                 }else if (never.isChecked()){
                     timeToRefresh = Integer.MAX_VALUE;
@@ -263,122 +240,46 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void refresh(View v){
+        progressBar.setVisibility(View.VISIBLE);
         refreshButton.setVisibility(View.INVISIBLE);
         changeVnumButton.setVisibility(View.INVISIBLE);
-        textView.setVisibility(View.INVISIBLE);
+        textView.setText("Your posts are being checked...");
         changeUpdateTimeButton.setVisibility(View.INVISIBLE);
         MailPage mailPage = new MailPage();
         mailPage.execute();
         executeWorkManager();
     }
-    public void loadAd(){
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-        adRequest = new AdRequest.Builder().build();
-
-        InterstitialAd.load(this,"ca-app-pub-8301793912429911/8053620032", adRequest,
-                new InterstitialAdLoadCallback() {
-                    @Override
-                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
-                        // The mInterstitialAd reference will be null until
-                        // an ad is loaded.
-                        mInterstitialAd = interstitialAd;
-                        fulscrn();
-                        //Log.i(TAG, "onAdLoaded");
-                    }
-
-                    @Override
-                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
-                        // Handle the error
-                        //Log.d(TAG, loadAdError.toString());
-                        mInterstitialAd = null;
-                    }
-                });
-
-
-    }
-    public void showAd(){
-        if (mInterstitialAd != null) {
-            mInterstitialAd.show(MainActivity.this);
-        } else {
-            // Log.d("TAG", "The interstitial ad wasn't ready yet.");
-        }
-    }
-    public void fulscrn(){
-        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
-            @Override
-            public void onAdClicked() {
-                // Called when a click is recorded for an ad.
-                //Log.d(TAG, "Ad was clicked.");
-            }
-
-            @Override
-            public void onAdDismissedFullScreenContent() {
-                // Called when ad is dismissed.
-                // Set the ad reference to null so you don't show the ad a second time.
-                //Log.d(TAG, "Ad dismissed fullscreen content.");
-                mInterstitialAd = null;
-            }
-
-            @Override
-            public void onAdFailedToShowFullScreenContent(AdError adError) {
-                // Called when ad fails to show.
-                //Log.e(TAG, "Ad failed to show fullscreen content.");
-                mInterstitialAd = null;
-            }
-
-            @Override
-            public void onAdImpression() {
-                // Called when an impression is recorded for an ad.
-                //Log.d(TAG, "Ad recorded an impression.");
-            }
-
-            @Override
-            public void onAdShowedFullScreenContent() {
-                // Called when ad is shown.
-                //Log.d(TAG, "Ad showed fullscreen content.");
-            }
-        });
-    }
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void ignoreBatteryOptimization() {
+        if (ContextCompat.checkSelfPermission(this,Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)!=
+                PackageManager.PERMISSION_DENIED || ContextCompat.checkSelfPermission(this,Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)!=
+                PackageManager.PERMISSION_DENIED){
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setMessage("You must allow permission this app to check your posts in background. Otherwise" +
+                    "you will not get notification even if you received a post!");
+            alert.setNegativeButton("Don't allow", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
 
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setMessage("You must allow permission this app to check your posts in background. Otherwise" +
-                "you will not get notification even if you received a post!");
-        alert.setNegativeButton("Don't allow", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
-        });
-        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                startPowerSaverIntent(MainActivity.this);
-                Intent intent = new Intent();
-                String packageName = getPackageName();
-                PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-                if (pm != null && !pm.isIgnoringBatteryOptimizations(packageName)) {
-                    intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-                    intent.setData(Uri.parse("package:" + packageName));
-                    startActivity(intent);
                 }
-            }
-        });
-        alert.show();
-
-
-
+            });
+            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    startPowerSaverIntent(MainActivity.this);
+                    Intent intent = new Intent();
+                    String packageName = getPackageName();
+                    PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+                    if (pm != null && !pm.isIgnoringBatteryOptimizations(packageName)) {
+                        intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                        intent.setData(Uri.parse("package:" + packageName));
+                        startActivity(intent);
+                    }
+                }
+            });
+            alert.show();
+        }
     }
-    public void runBackGround(View v){
-        ignoreBatteryOptimization();
-    }
-
-
     public static List<Intent> POWER_MANAGER_INTENTS = Arrays.asList(
             new Intent().setComponent(new ComponentName("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity")),
             new Intent().setComponent(new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity")),
@@ -391,8 +292,6 @@ public class MainActivity extends AppCompatActivity {
             new Intent().setComponent(new ComponentName("com.vivo.permissionmanager", "com.vivo.permissionmanager.activity.BgStartUpManagerActivity")),
             new Intent().setComponent(new ComponentName("com.asus.mobilemanager", "com.asus.mobilemanager.entry.FunctionActivity")).setData(android.net.Uri.parse("mobilemanager://function/entry/AutoStart"))
     );
-
-
     public static void startPowerSaverIntent(Context context) {
         SharedPreferences settings = context.getSharedPreferences("ProtectedApps", Context.MODE_PRIVATE);
         boolean skipMessage = settings.getBoolean("skipProtectedAppCheck", false);
@@ -425,8 +324,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-
     private static boolean isCallable(Context context, Intent intent) {
         List<ResolveInfo> list = context.getPackageManager().queryIntentActivities(intent,
                 PackageManager.MATCH_DEFAULT_ONLY);
